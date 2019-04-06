b22e0a88de784d06ffa35d289b66a2436de45860
Jamie Sanson <jamie.h.sanson@gmail.com>
2019-04-07 11:48:21 +1200

    Replace all icons with round versions and changes Content warning input

--- toot/src/main/java/io/github/koss/mammut/toot/model/Extensions.kt
+++ toot/src/main/java/io/github/koss/mammut/toot/model/Extensions.kt
@@ -6,6 +6,6 @@ import io.github.koss.mammut.toot.R
 val (Status.Visibility).iconRes get() = when (this) {
     Status.Visibility.Public -> R.drawable.ic_public_black_24dp
     Status.Visibility.Unlisted -> R.drawable.ic_lock_open_black_24dp
-    Status.Visibility.Private -> R.drawable.ic_lock_outline_black_24dp
+    Status.Visibility.Private -> R.drawable.ic_lock_black_24dp
     Status.Visibility.Direct -> R.drawable.ic_mail_black_24dp
 }
